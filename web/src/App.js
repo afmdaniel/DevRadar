3e48f3bb8465303cff7f8c9c82e6698e731d8f8c
Daniel Melo <danielfarias_21@hotmail.com>
2020-01-15 22:52:54 -0300

    final 3

--- web/src/App.js
+++ web/src/App.js
@@ -6,142 +6,40 @@ import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
 function App() {
-  const [github_username, setGithub_username] = useState('');
-  const [techs, setTechs] = useState('');
-  const [longitude, setLongitude] = useState('');
-  const [latitude, setLatitude] = useState('');
+  const [devs, setDevs] = useState([]);
   
-  async function handleAddDev(e) {
-    e.preventDefault();
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
 
-    const response = await api.post('/devs', {
-      github_username,
-      techs, 
-      latitude,
-      longitude,
-    })
-
-    setGithub_username('');
-    setTechs('');
+    setDevs([...devs, response.data]);
   }
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-
-        setLatitude(latitude);
-        setLongitude(longitude);
-    },
-    (err) => {
-      console.log(err)
-    },
-    {
-      timeout: 30000,
+    async function loadDevs(){
+      const response = await api.get('/devs');
+      setDevs(response.data)
     }
-    );
-  }, []);
+
+    loadDevs();
+  }, [])
+  // passo o array vazio se quero que execute apenas uma unica vez
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input
-              name="github_username"
-              id="github_username"
-              required
-              value={github_username}
-              onChange={e => setGithub_username(e.target.value)}/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input
-              name="techs"
-              id="techs"
-              required
-              value={techs}
-              onChange={e => setTechs(e.target.value)}/>
-          </div>
-
-          <div className="input-group">  
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                type="number"
-                name="latitude" 
-                id="latitude"
-                required
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}/>
-            </div>
-            
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                type="number"
-                name="longitude"
-                id="longitude"
-                required
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}/>
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
       
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/59712925?s=460&v=4" alt="Daniel Melo"/>
-              <div className="user-info">
-                <strong>Daniel Melo</strong>
-                <span>ReactJs, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Jogo bola e danço</p>
-            <a href="https://github.com/afmdaniel">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/59712925?s=460&v=4" alt="Daniel Melo"/>
-              <div className="user-info">
-                <strong>Daniel Melo</strong>
-                <span>ReactJs, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Jogo bola e danço</p>
-            <a href="https://github.com/afmdaniel">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/59712925?s=460&v=4" alt="Daniel Melo"/>
-              <div className="user-info">
-                <strong>Daniel Melo</strong>
-                <span>ReactJs, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Jogo bola e danço</p>
-            <a href="https://github.com/afmdaniel">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/59712925?s=460&v=4" alt="Daniel Melo"/>
-              <div className="user-info">
-                <strong>Daniel Melo</strong>
-                <span>ReactJs, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Jogo bola e danço</p>
-            <a href="https://github.com/afmdaniel">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
